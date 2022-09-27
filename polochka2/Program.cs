4ba0f84db4b9c22da05ae02b2047845150009f40
Студент KSI <student@ksi.esstu.ru>
2022-09-27 16:56:53 +0800

    polochka2

---
+++ polochka2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace polochka2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Elena!");
+        }
+    }
+}

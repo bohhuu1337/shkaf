ccad71642539abe71c420f594e342f60c156804c
Студент KSI <student@ksi.esstu.ru>
2022-09-27 16:43:03 +0800

    polochka1

---
+++ polochka1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace polochka1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
